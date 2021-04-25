bd865117719718a26460ba9c42717daa1036f654
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-04-25 11:59:02 -0700

    last changes before realese

--- src/components/content/about/About.js
+++ src/components/content/about/About.js
@@ -10,7 +10,11 @@ const About = () => {
                 <p>This Domain is being used just as the portfolio for Hooman Hajarian.</p>
             </Typography>
             <Typography>
-                <p>The website is coded using React running on a docker container.</p>
+                <p>The web app is coded using React running on a docker container.</p>
+            </Typography>
+            <Typography>
+                <p>GitHub page is open to public, so feel free to clone and do your changes if you find it useful:
+                <a href="https://github.com/hoomanhajrian/portfolio.git">https://github.com/hoomanhajrian/portfolio.git</a></p>
             </Typography>
         </div>
     )
