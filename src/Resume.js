c1a1ec062fa4f544556f8df901e0d3688a27d42b
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-05-30 22:47:29 -0700

    replacing resume

--- src/Resume.js
+++ src/Resume.js
@@ -12,25 +12,65 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Contact from './components/header/Contact';
 import About from './components/content/about/About';
+import Technical from './components/content/contents/technical/Technical';
+import Work from './components/content/contents/work/Work';
+import Education from './components/content/contents/education/Education';
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
 
 
 
 const App = () => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
   const [state, setState] = React.useState({
     checkedB: false,
   });
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      backgroundColor: theme.palette.background.paper,
-      width: 500,
-    },
-  }));
-
-
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
 
   function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -80,38 +120,35 @@ const App = () => {
   return (
 
     <div className="resume">
-
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Portfolio" {...a11yProps(0)} />
-          <Tab label="Contact Me" {...a11yProps(1)} />
-          <Tab label="About" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          <Content />
-          <Footer />
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          <Contact />
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          <About />
-        </TabPanel>
-      </SwipeableViews>
+      <div className="projects">
+        <h2 className="projects-header">Resume</h2>
+        <div className="cards-container">
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            className={classes.tabs}
+          >
+            <Tab label="Skills" {...a11yProps(0)} />
+            <Tab label="Work Experience" {...a11yProps(1)} />
+            <Tab label="Education" {...a11yProps(2)} />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            <Technical />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <Work />
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <Education />
+          </TabPanel>
+        </div>
+      </div>
     </div>
+
+
   );
 }
 
--- src/components/content/contents/projects/Projects.js
+++ src/components/content/contents/projects/Projects.js
@@ -7,90 +7,159 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Box from '@material-ui/core/Box';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
 
-function Projects() {
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        display: 'flex',
+        height: 224,
+    },
+    tabs: {
+        borderRight: `1px solid ${theme.palette.divider}`,
+    },
+}));
+
+const Projects = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <div className="projects">
             <h2 className="projects-header">Project Experience</h2>
             <div className="cards-container">
-                <Card className="project-card card1">
-                    <CardMedia
-                        className="card-media"
-                        image="./farmerfresh.jpg"
-                        title="farmer fresh project"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h4" component="h2">
-                            Farmer Fresh
+                <Tabs
+                    orientation="vertical"
+                    variant="scrollable"
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="Vertical tabs example"
+                    className={classes.tabs}
+                >
+                    <Tab label="Farmer Fresh" {...a11yProps(0)} />
+                    <Tab label="Patch-App" {...a11yProps(1)} />
+                    <Tab label="Go Hike" {...a11yProps(2)} />
+                </Tabs>
+                <TabPanel value={value} index={0}>
+                    <Card className="project-card card1">
+                        <CardMedia
+                            className="card-media"
+                            image="./farmerfresh.jpg"
+                            title="farmer fresh project"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h4" component="h2">
+                                Farmer Fresh
                             </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Project Manager/Full Stack Developer
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Project Manager/Full Stack Developer
                             </Typography>
-                        <Typography gutterBottom variant="h6" component="h2">
-                            2021 - 2022
+                            <Typography gutterBottom variant="h6" component="h2">
+                                2021 - 2022
                             </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            <a href="https://farmerfresh.ca/" target="_blank">farmerfresh.ca</a>
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Java based mobile application that help farmers share their products and consumers are able to put an order for pick up or delivery.
+                            <Typography gutterBottom variant="h5" component="h2">
+                                <a href="https://farmerfresh.ca/" target="_blank">farmerfresh.ca</a>
                             </Typography>
-                    </CardContent>
-                </Card>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Java based mobile application that help farmers share their products and consumers are able to put an order for pick up or delivery.
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <Card className="project-card card2">
 
-                <Card className="project-card card2">
+                        <CardMedia
+                            className="card-media"
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h4" component="h2">
+                                Patch App
+        </Typography>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Full Stack Developer
+        </Typography>
+                            <Typography gutterBottom variant="h6" component="h2">
+                                2020 - 2021
+        </Typography>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                <a href="https://patch-app.ca/" target="_blank">patch-app.ca</a>
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Online Platform for kids to learn, how to fix injuries in case of emergency. This platfrom was based on React Technology with back end on AWS servers.
+        </Typography>
+                        </CardContent>
 
-                    <CardMedia
-                        className="card-media"
-                        image="/static/images/cards/contemplative-reptile.jpg"
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h4" component="h2">
-                            Patch App
-                            </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Full Stack Developer
-                            </Typography>
-                        <Typography gutterBottom variant="h6" component="h2">
-                            2020 - 2021
-                            </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            <a href="https://patch-app.ca/" target="_blank">patch-app.ca</a>
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Online Platform for kids to learn, how to fix injuries in case of emergency. This platfrom was based on React Technology with back end on AWS servers.
-                            </Typography>
-                    </CardContent>
+                    </Card>
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    <Card className="project-card card3">
 
-                </Card>
-
-                <Card className="project-card card3">
-
-                    <CardMedia
-                        className="card-media"
-                        image="/static/images/cards/contemplative-reptile.jpg"
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h4" component="h2">
-                            Go Hike
+                        <CardMedia
+                            className="card-media"
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h4" component="h2">
+                                Go Hike
+        </Typography>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Full Stack Developer
+        </Typography>
+                            <Typography gutterBottom variant="h6" component="h2">
+                                2020 - 2021
+        </Typography>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                <a href="http://gohike.ca/" target="_blank">goHike.ca</a>
                             </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            Full Stack Developer
-                            </Typography>
-                        <Typography gutterBottom variant="h6" component="h2">
-                            2020 - 2021
-                            </Typography>
-                        <Typography gutterBottom variant="h5" component="h2">
-                            <a href="http://gohike.ca/" target="_blank">goHike.ca</a>
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                            Cordova based mobile application that shows the hiking trails around the user and user is able to see the route to the trail and the trail route it self on the map.
-                            </Typography>
-                    </CardContent>
-                </Card>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                Cordova based mobile application that shows the hiking trails around the user and user is able to see the route to the trail and the trail route it self on the map.
+        </Typography>
+                        </CardContent>
+                    </Card>
+                </TabPanel>
             </div>
         </div>
     )
