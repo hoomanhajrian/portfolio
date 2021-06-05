8ccc30854b26df52e9fa95592194d65eda0fa342
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-06-05 02:12:17 -0700

    last changes before publish contact needs a form

--- src/components/header/Contact.js
+++ src/components/header/Contact.js
@@ -7,13 +7,13 @@ function Contact() {
 
     return (
         <div className="contact">
-            <h1>Contact me</h1>
+            <h2>Contact me</h2>
             <ul className="links">
-                <li><h4>LinkedIn:</h4></li>
+                <li><h6>LinkedIn:</h6></li>
                 <li><a href="https://www.linkedin.com/in/hooman-hajarian/" target="_blank"><i class="fab fa-linkedin"></i><span className="visually-hidden">https://www.linkedin.com/in/hooman-hajarian/</span></a></li>
-                <li><h4>Email:</h4></li>
+                <li><h6>Email:</h6></li>
                 <li>hajarianhooman@gmail.com</li>
-                <li><h4>GitHub:</h4></li>
+                <li><h6>GitHub:</h6></li>
                 <li><a href="https://github.com/hoomanhajrian" target="_blank"><i class="fab fa-github-square"></i><span className="visually-hidden">https://github.com/hoomanhajrian</span></a></li>
             </ul>
         </div>
