2b81ba1e186d149feaf46f76657c34ed35fb2b25
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2021-12-15 22:30:13 -0800

    added next and pre functionalities

--- src/components/showoff/showoffComponents/MusicPlayer.js
+++ src/components/showoff/showoffComponents/MusicPlayer.js
@@ -31,13 +31,34 @@ const sampleMusics = [
         image: "image",
         length: 146
     },
+    {
+        id: 4,
+        name: "Track Four",
+        artist: "Lonely One",
+        color: "purple",
+        image: "image",
+        length: 146
+    },
+    {
+        id: 5,
+        name: "Track Five",
+        artist: "Firefly",
+        color: "yellow",
+        image: "image",
+        length: 146
+    },
 ];
 
-const MusicPlayer = () => {
-    const [barLoc, changeBarLoc] = useState('');
-    const [music, changeMusic] = useState('');
-    const [loadedList, changeList] = useState(sampleMusics);
+const musicIds = [];
+sampleMusics.forEach(x => musicIds.push(x.id));
+console.log(musicIds);
 
+
+const MusicPlayer = () => {
+    const [barLoc, changeBarLoc] = useState(0);
+    const [selectedMusic, changeSelectedMusic] = useState(5);
+    const [loadedList, changeList] = useState(sampleMusics);
+    const random = false;
 
     const changeBar = (e) => {
         changeBarLoc(e.target.value);
@@ -54,21 +75,50 @@ const MusicPlayer = () => {
         console.log("Pause" + barLoc);
     };
 
+    const previous = () => {
+
+        if (musicIds.includes(selectedMusic - 1) || (musicIds.includes(selectedMusic + 1) && selectedMusic !== musicIds[musicIds.length - 1] && selectedMusic !== musicIds[0])) {
+            changeSelectedMusic(selectedMusic - 1);
+        }
+        else {
+            changeSelectedMusic(musicIds.length);
+        }
+    };
+
+    const next = () => {
+        if (selectedMusic < musicIds.length && selectedMusic > musicIds[0]) {
+            changeSelectedMusic(selectedMusic + 1);
+        }
+        else if (selectedMusic === musicIds.length) {
+            changeSelectedMusic(1);
+        }
+        else if (selectedMusic === musicIds[0]) {
+            changeSelectedMusic(selectedMusic + 1);
+        }
+        else {
+            changeSelectedMusic(1);
+        }
+    };
+
+    const repeat = () => {
+
+    };
+
     return (
         <Card className="music-card">
             <CardHeader
                 title="Music Player"
-                subheader={loadedList[0].name}
+                subheader={loadedList[selectedMusic - 1].name}
                 className="music-card-header"
                 style={{ color: "white" }}
             />
 
             {
-                music ? <CardMedia
+                selectedMusic ? <CardMedia
                     component="img"
                     height="100"
-                    image={music.pic}
-                    alt={music.pic}
+                    image={loadedList[selectedMusic - 1].pic}
+                    alt={loadedList[selectedMusic - 1].pic}
                 /> : <p>Empty Player</p>
             }
             <Slider
@@ -78,20 +128,22 @@ const MusicPlayer = () => {
                 onChange={changeBar}
                 step={1}
                 min={0}
-                max={loadedList[0].length}
+                max={loadedList[selectedMusic - 1].length}
                 valueLabelDisplay="auto"
-            />
-            <Typography>{`${Math.floor(barLoc / 60)}:${(barLoc % 60)}`}</Typography>
+            /><div className="music-time-wrapper">
+                <Typography>{`${Math.floor(barLoc / 60)}:${(barLoc % 60)}`}</Typography>
+                <Typography>{`${Math.floor(loadedList[selectedMusic - 1].length / 60)}:${(loadedList[selectedMusic - 1].length % 60)}`}</Typography>
+            </div>
             <div className="music-controller-container">
-                <Button className="music-controller-button">Random</Button>
-                <Button className="music-controller-button">Pre</Button>
+                <Button onClick={!random} className="music-controller-button">Random</Button>
+                <Button onClick={previous} className="music-controller-button">Pre</Button>
                 <Button onClick={play} className="music-controller-button">Play</Button>
                 <Button onClick={pause} className="music-controller-button">Pause</Button>
-                <Button className="music-controller-button">Next</Button>
-                <Button className="music-controller-button">Repeat</Button>
+                <Button onClick={next} className="music-controller-button">Next</Button>
+                <Button onClick={repeat} className="music-controller-button">Repeat</Button>
             </div>
 
-        </Card>
+        </Card >
     )
 
 
