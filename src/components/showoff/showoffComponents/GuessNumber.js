ea7600072d5faab2f8b717f41f6e146b236276c0
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2022-03-25 12:15:11 -0700

    added guess number component

---
+++ src/components/showoff/showoffComponents/GuessNumber.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from '@material-ui/core';
+
+const GuessNumber = () => {
+
+    return (
+        <Card className='guess-number'>
+
+        </Card>
+    )
+
+};
