71ef6ae2e6c203a5a861aaced7f8294ffa0b689c
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2022-04-19 10:44:58 -0700

    fixed menu styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './sass/main.scss';
 import {
     HashRouter as Router,
@@ -6,69 +6,28 @@ import {
     Route,
     NavLink
 } from "react-router-dom";
-import Header from './components/header/Header';
 import Contact from './components/contact/Contact'
 import Projects from './components/content/contents/projects/Projects'
 import Resume from './components/resume/Resume';
 import Footer from './components/footer/Footer';
 import Showoff from './components/showoff/showoff';
 import NotFound from './components/notfound/NotFound';
-import { makeStyles } from '@mui/styles';
 
 
 
 const App = () => {
-    const [anchorEl, changeAnchorEl] = useState(null)
-
-
-    const handleClick = event => {
-        changeAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        changeAnchorEl(null);
-    };
-
 
     return (
         <div className="app">
             <Router>
-                {/* <List className={classes.header}>
-                    <ListItem>
-                        <NavLink to="/"><Header /></NavLink>
-                    </ListItem>
-                    <Button
-                        aria-owns={anchorEl ? 'portfolio-menu' : undefined}
-                        aria-haspopup="true"
-                        onClick={handleClick}
-                        className={classes.menuButton}
-                    >
-                        Menu
-                    </Button>
-                    <Menu
-                        id="portfolio-menu"
-                        anchorEl={anchorEl}
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onClick={handleClose} className={classes.menuItem}><NavLink className={classes.menuLink} activeClassName={classes.activeLink} exact to="/">Projects</NavLink></MenuItem>
-
-                        <MenuItem onClick={handleClose} className={classes.menuItem}><NavLink className={classes.menuLink} activeClassName={classes.activeLink} to="/resume">Resume</NavLink></MenuItem>
-
-                        <MenuItem onClick={handleClose} className={classes.menuItem}><NavLink className={classes.menuLink} activeClassName={classes.activeLink} to="/showoff">Showoff</NavLink></MenuItem>
-
-                        <MenuItem onClick={handleClose} className={classes.menuItem}><NavLink className={classes.menuLink} activeClassName={classes.activeLink} to="/about">About</NavLink></MenuItem>
-                    </Menu>
-                </List>
-                 */}
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="/">Hooman Hajarian</a>
-                        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="/">Hooman Hajarian</a>
+                        <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="navbar-collapse collapse" id="navbarColor01">
-                            <ul class="navbar-nav me-auto">
+                        <div className="navbar-collapse collapse" id="navbarColor01">
+                            <ul className="navbar-nav me-auto">
                                 <li className="nav-item">
                                     <NavLink exact className="nav-link" to="/">Projects</NavLink>
                                 </li>
@@ -94,7 +53,7 @@ const App = () => {
                 </NavSwitch>
             </Router>
             <Footer />
-        </div>
+        </div >
     )
 };
 
