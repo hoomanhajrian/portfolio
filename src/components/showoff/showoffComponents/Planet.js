08908740139ad93c94405c99623fde13ef003872
hoomanhajrian <66041489+hoomanhajrian@users.noreply.github.com>
2022-09-19 01:04:05 -0700

    change name to solar system and added rings and names

--- src/components/showoff/showoffComponents/Planet.js
+++ src/components/showoff/showoffComponents/Planet.js
@@ -1,6 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from "three";
+// import { Text } from '@react-three/drei';
+import Ring from './Ring';
 
 const Planet = (props) => {
 
@@ -9,25 +11,31 @@ const Planet = (props) => {
     // This reference gives us direct access to the THREE.Mesh object
     const ref = useRef()
     // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
+    // const [hovered, hover] = useState(false)
     // Subscribe this component to the render-loop, rotate the mesh every frame
     let t = 0;
     useFrame((state, delta) => {
         t += props.speed ? props.speed : null;
         ref.current.position.x = props.rotationValue * Math.cos(t) + 0;
         ref.current.position.z = props.rotationValue * Math.sin(t) + 0; // These to strings make it work
+        ref.current.rotation.y += .002;
     })
+
+
     // Return the view, these are regular Threejs elements expressed in JSX
     return (
         <mesh
             {...props}
             ref={ref}
-            onClick={(event) => click(!clicked)}
-            onPointerOver={(event) => hover(true)}
-            onPointerOut={(event) => hover(false)}>
-            <sphereGeometry />
+        // onPointerOver={(event) => hover(true)}
+        // onPointerOut={(event) => hover(false)}
+        >
+            <sphereGeometry attach='geometry' />
             <meshLambertMaterial color={props.color} map={texture} />
+            {props.name === 'Saturn' ? <Ring texture={true} position={props.position} scale={[5, 5, 5]} attributes={[0.25, 0.35, 128]} /> : ''}
+            {/* <Text color="white" position={[0, 1.5, 0]} scale={[3, 3, 3]}>
+                {props.name}
+            </Text> */}
         </mesh>
     )
 };
--- src/components/showoff/showoffComponents/Ring.js
+++ src/components/showoff/showoffComponents/Ring.js
@@ -2,11 +2,13 @@ import React, { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from "three";
 
+
+const saturnRingTexture = new THREE.TextureLoader().load('/textures/saturn_ring.png');
+
 const Ring = (props) => {
     // This reference gives us direct access to the THREE.Mesh object
     const ref = useRef()
     // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
     const [clicked, click] = useState(false)
     // Subscribe this component to the render-loop, rotate the mesh every frame
     useFrame((state, delta) => {
@@ -18,14 +20,13 @@ const Ring = (props) => {
     return (
         <mesh
             {...props}
+            position={[0, 0, 0]}
             rotation={[-90, 0, 0]}
             ref={ref}
             onClick={(event) => click(!clicked)}
-            onPointerOver={(event) => hover(true)}
-            onPointerOut={(event) => hover(false)}
         >
             <ringGeometry args={props.attributes} />
-            <meshLambertMaterial side={THREE.DoubleSide} color={props.color} />
+            <meshLambertMaterial side={THREE.DoubleSide} color={props.color} map={props.texture ? saturnRingTexture : null} opacity={props.texture ? 0.9 : 1} transparent />
         </mesh>
     )
 };
