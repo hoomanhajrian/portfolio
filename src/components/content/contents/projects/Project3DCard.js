8887068b1a0ec80d349ecce7affca926e7ef3f3e
Hooman Hajarian <hajarianhooman@gmail.com>
2024-02-05 00:10:28 -0800

    moving 3d cards with shadows

--- src/components/content/contents/projects/Project3DCard.js
+++ src/components/content/contents/projects/Project3DCard.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
-import { useFrame,useThree } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
 import { Html, Text, RoundedBox } from "@react-three/drei";
+import { keyframes } from "styled-components";
 
 const Project3DCard = ({ position, data }) => {
   const cardContainerRef = useRef();
@@ -9,32 +10,39 @@ const Project3DCard = ({ position, data }) => {
   const [clicked, click] = useState(false);
 
   useFrame(({ gl, scene, camera }) => {
-    if (cardContainerRef.current.position.x <= position[0] + 30) {
-      cardContainerRef.current.position.x = cardContainerRef.current.position.x + 0.1;
-      cardContainerRef.current.position.z = cardContainerRef.current.position.z + 0.1;
+    if (camera.position.x > -5) {
+      // cardContainerRef.current.position.x =
+      //   cardContainerRef.current.position.x + 0.1;
+      cardContainerRef.current.position.z =
+        cardContainerRef.current.position.z + 0.01;
       camera.position.z = camera.position.z + 0.001;
       camera.position.x = camera.position.x - 0.004;
+      camera.rotation.y = camera.rotation.y - 0.0001;
     }
-  })
-
-  // const tryAnimation = ()=>{
-  //   let {camera} = useThree();
-    
-  // };
+  });
 
   return (
-    <group ref={cardContainerRef}
-      onPointerOver={tryAnimation}
-      onPointerOut={(event) => { hover(false);}}>
+    <group
+      castShadow
+      args={[3, 5, 0.3]} // Width, height, depth. Default is [1, 1, 1]
+      ref={cardContainerRef}
+      onPointerOver={(event) => {
+        hover(true);
+      }}
+      onPointerOut={(event) => {
+        hover(false);
+      }}
+    >
       <RoundedBox
+        castShadow
         ref={card}
-        position={[-40 + position[0], position[1], -30 + position[2]]}
+        position={[position[0], position[1], position[2]]}
         args={[3, 5, 0.3]} // Width, height, depth. Default is [1, 1, 1]
         radius={0.05} // Radius of the rounded corners. Default is 0.05
-        smoothness={4} // The number of curve segments. Default is 4
+        smoothness={10} // The number of curve segments. Default is 4
         bevelSegments={4} // The number of bevel segments. Default is 4, setting it to 0 removes the bevel, as a result the texture is applied to the whole geometry.
         creaseAngle={0.4} // Smooth normals everywhere except faces that meet at an angle greater than the crease angle
-        scale={hovered ? 3 : 1}
+        // scale={hovered ? 3 : 1}
         onClick={(event) => click(!clicked)}
       >
         <meshPhongMaterial color="lightblue" />
--- src/components/content/contents/projects/Projects.js
+++ src/components/content/contents/projects/Projects.js
@@ -1,6 +1,14 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState, useMemo, useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Billboard, Html, RoundedBox } from "@react-three/drei";
+import {
+  Billboard,
+  Html,
+  RoundedBox,
+  OrbitControls,
+  Polyhedron,
+} from "@react-three/drei";
+
+import * as THREE from "three";
 import Project2DCard from "./Project2DCard";
 import Project3DCard from "./Project3DCard";
 
@@ -15,7 +23,7 @@ const projectsData = [
       "Cordova-based mobile application that shows the hiking trails around the user and the user is able to see the route to the trail and the trail route itself on the map.",
     imgUrl: "/img/gohike.jpg",
     gitHub: "https://github.com/costa-rodrigo/goHike",
-    position3D: [-4, 0, 0],
+    position3D: [-4, 0, 20],
   },
   {
     id: 1,
@@ -27,7 +35,7 @@ const projectsData = [
       "Java based mobile application that help farmers share their products and consumers are able to put an order for pick up or delivery.",
     imgUrl: "/img/farmerfresh.jpg",
     gitHub: "https://github.com/hoomanhajrian/FarmerFresh_android-app",
-    position3D: [0, 0, 0],
+    position3D: [0, 0, 20],
   },
   {
     id: 2,
@@ -39,7 +47,7 @@ const projectsData = [
       "Online Platform for kids to learn, how to fix injuries in case of emergency. This platform was based on React Technology with the back end on AWS servers.",
     imgUrl: "/img/patchapp.jpg",
     gitHub: "https://github.com/costa-rodrigo/patch-frontend",
-    position3D: [4, 0, 0],
+    position3D: [4, 0, 20],
   },
 
   {
@@ -52,7 +60,7 @@ const projectsData = [
       "React based application using movie API and more features such as watching and commenting on the movies will be added later.",
     imgUrl: "/img/movie-time.jpg",
     gitHub: "https://github.com/hoomanhajrian/Moive-app",
-    position3D: [-4, -6, 0],
+    position3D: [-4, -6, 20],
   },
   {
     id: 4,
@@ -64,7 +72,7 @@ const projectsData = [
       "We are here to make sure your event planning will go as perfect as it can be with the least affort using this platform. You can estimate your event total cost and book appointment for consultation about your event.",
     imgUrl: "/img/wedding.jpg",
     gitHub: "https://github.com/hoomanhajrian/EventPlanner",
-    position3D: [0, -6, 0],
+    position3D: [0, -6, 20],
   },
   {
     id: 5,
@@ -76,7 +84,7 @@ const projectsData = [
       "Platform for customizing your own business card and ordering it with QR code and NFC features that you can add to your card.",
     imgUrl: "/img/card-creator.jpg",
     gitHub: "https://github.com/hoomanhajrian/card-creator",
-    position3D: [4, -6, 0],
+    position3D: [4, -6, 20],
   },
   {
     id: 6,
@@ -88,7 +96,7 @@ const projectsData = [
       "Online store for selling industrial kitchen equipment coded fully in React using Redux and many other libraries.(Site in farsi language)",
     imgUrl: "/img/tajhizaras.jpg",
     gitHub: "https://github.com/hoomanhajrian/tajhizaras",
-    position3D: [-4, 6, 0],
+    position3D: [-4, 6, 20],
   },
   {
     id: 7,
@@ -100,7 +108,7 @@ const projectsData = [
       "Online web application for small stores accounting and inventory management using React(TypeScript) and Nodejs",
     imgUrl: "/img/hesab.jpg",
     gitHub: "https://github.com/hoomanhajrian/hesabketab",
-    position3D: [0, 6, 0],
+    position3D: [0, 6, 20],
   },
   {
     id: 8,
@@ -112,7 +120,7 @@ const projectsData = [
       "Web site for photographer introduction and portfolio and resume using Nextjs and Nodejs.",
     imgUrl: "/img/lapsemoon.jpeg",
     gitHub: "https://github.com/hoomanhajrian/lapsemoon",
-    position3D: [4, 6, 0],
+    position3D: [4, 6, 20],
   },
   {
     id: 9,
@@ -124,7 +132,7 @@ const projectsData = [
       "Online Platform to connect web customers to Amazon Market place and the business social media.",
     imgUrl: "/img/littesellca.jpg",
     gitHub: "https://github.com/hoomanhajrian/littlesellca",
-    position3D: [-8, 6, 0],
+    position3D: [-8, 6, 20],
   },
   {
     id: 10,
@@ -136,7 +144,7 @@ const projectsData = [
       "Website using Next.js technology using react for front end and simple mail service for backend all server side rendered.",
     imgUrl: "/img/pacivil.jpg",
     gitHub: "https://github.com/hoomanhajrian/pacivil",
-    position3D: [-8, 0, 0],
+    position3D: [-8, 0, 20],
   },
 ];
 
@@ -167,17 +175,25 @@ const Projects = () => {
     return (
       <Suspense fallback={<p>loading...</p>}>
         <Canvas
-          camera={{ position: [18, 6, 25], fov: 50, angel: 0 }}
+          shadows
+          camera={{ position: [15, 0, 40], fov: 50, angel: 0 }}
           style={{
             width: "100%",
             height: "90vh",
           }}
         >
-          <ambientLight intensity={1.2} />
-          <pointLight position={[-18, 6, 19]} />
-          <Billboard position={[-10, 1.7, 16]}>
+          {/* <OrbitControls /> */}
+          <spotLight
+            angle={"40"}
+            castShadow
+            position={[0, 6, 40]}
+            color="lightblue"
+            fov={"90"}
+          />
+
+          <Billboard position={[12, 10, 20]}>
             <Html>
-              <h1 style={{color:'white'}}>Projects</h1>
+              <h2 style={{ color: "white", fontSize: "3rem" }}>Projects</h2>
             </Html>
           </Billboard>
           {projectsData.map((cardData) => {
@@ -189,16 +205,17 @@ const Projects = () => {
               />
             );
           })}
-          <group >
+          <group receiveShadow>
             <RoundedBox
-              position={[-50, 0, -50]}
-              args={[100, 100, 0.5]} // Width, height, depth. Default is [1, 1, 1]
+              receiveShadow
+              position={[0, 0, 0]}
+              args={[200, 200, 0.5]} // Width, height, depth. Default is [1, 1, 1]
               radius={0.05} // Radius of the rounded corners. Default is 0.05
               smoothness={4} // The number of curve segments. Default is 4
               bevelSegments={4} // The number of bevel segments. Default is 4, setting it to 0 removes the bevel, as a result the texture is applied to the whole geometry.
               creaseAngle={0.4} // Smooth normals everywhere except faces that meet at an angle greater than the crease angle
             >
-              <meshPhongMaterial color="#000" />
+              <meshPhongMaterial color="#fff" />
             </RoundedBox>
           </group>
         </Canvas>
