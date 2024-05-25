6a416c1e2198ba9d8affa599e934eb20fa4bbbf4
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-05-25 18:25:27 +0000

    updated pdf file source

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,127 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Technical from '../content/contents/technical/Technical';
-import Work from '../content/contents/work/Work';
-import Education from '../content/contents/education/Education';
+// Import the main component
+import { Viewer } from '@react-pdf-viewer/core';
+import { Worker } from '@react-pdf-viewer/core';
 
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
+
+const Resume = () => {
   return (
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
     <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
+      style={{
+        border: '5px solid #2566e8',
+        height: '90vh',
+      }}
     >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      <Viewer fileUrl="/resume.pdf" />
     </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
+    </Worker>
+  )
 };
 
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-    display: 'flex',
-    height: 224,
-  },
-  tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`,
-  },
-}));
-
-
-
-const App = () => {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`full-width-tabpanel-${index}`}
-        aria-labelledby={`full-width-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box p={3}>
-            <Typography component='div'>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-  };
-
-  function a11yProps(index) {
-    return {
-      id: `full-width-tab-${index}`,
-      'aria-controls': `full-width-tabpanel-${index}`,
-    };
-  }
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <div className="resume">
-      <h2 className="resume-header">Resume</h2>
-      <div className="cards-container">
-        <Tabs
-          orientation="vertical"
-          variant="scrollable"
-          value={value}
-          onChange={handleChange}
-          aria-label="Vertical tabs example"
-          className={classes.tabs}
-        >
-          <Tab label="Skills" {...a11yProps(0)} />
-          <Tab label="Work Experience" {...a11yProps(1)} />
-          <Tab label="Education" {...a11yProps(2)} />
-        </Tabs>
-        <TabPanel value={value} index={0}>
-          <Technical />
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <Work />
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <Education />
-        </TabPanel>
-      </div>
-    </div>
-
-
-  );
-}
-
-export default App;
+export default Resume;
