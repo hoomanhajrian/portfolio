3a036d8d31fe756c7baa25e81f27410535895040
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-07-03 03:49:07 +0000

    added download resume button

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,6 +1,8 @@
 // Import the main component
 import { Viewer } from '@react-pdf-viewer/core';
 import { Worker } from '@react-pdf-viewer/core';
+import { Button } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
 
 // Import the styles
 import '@react-pdf-viewer/core/lib/styles/index.css';
@@ -8,16 +10,23 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 
 const Resume = () => {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-    <div
-      style={{
-        border: '5px solid #2566e8',
-        height: '90vh',
-      }}
-    >
-      <Viewer fileUrl="/resume.pdf" />
+    <div style={{ backgroundColor: '#2566e8', width: '100%' }}>
+      <Button href='/resume.pdf' download sx={{
+        margin: '1rem', borderRadius: '15px', '&:hover': {
+          backgroundColor: '#fff',
+        }
+      }} variant='contained' color='success'><DownloadIcon /> Download Resume</Button>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <div
+          style={{
+            border: '5px solid #2566e8',
+            height: '90vh',
+          }}
+        >
+          <Viewer fileUrl="/resume.pdf" />
+        </div>
+      </Worker>
     </div>
-    </Worker>
   )
 };
 
