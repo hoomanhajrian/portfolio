345a0e4b88056a93d5cdacd8d2579a69901ec194
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-09-02 22:27:41 +0000

    character goes forward and backwards

--- src/components/content/game/Game.js
+++ src/components/content/game/Game.js
@@ -1,15 +1,19 @@
-import React, { useEffect, useState,Suspense } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls,PerspectiveCamera,Plane } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Plane } from "@react-three/drei";
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
-import {Running} from './model/Running';
+import { Running } from './model/Running';
+import { Model } from "./model/Model";
+import { Walls } from "../../walls/Walls";
 
-const Game = ()=>{
-    const [projects, showProjects] = useState(false);
+const Game = () => {
+  const [modelPos,updateModelPos] = useState([0,-15,0]);
+  const [modelDir,updateModelDir] = useState(0);
+  const [modelState, updateModelState] = useState('idle');
   const [switchState, updateSwitch] = useState(false);
   // loading textures
-  const [wallTexture, wallRoughness, groundTexture,groundRoughness] = useLoader(TextureLoader, ["/textures/wall/brick-wall.jpg", "/textures/wall/brick-wall-rough.jpg"
-    , "/textures/ground/asphalt.jpg","/textures/ground/asphalt-rough.jpg"])
+  const [wallTexture, wallRoughness, groundTexture, groundRoughness] = useLoader(TextureLoader, ["/textures/wall/brick-wall.jpg", "/textures/wall/brick-wall-rough.jpg"
+    , "/textures/ground/asphalt.jpg", "/textures/ground/asphalt-rough.jpg"])
   // screen dimentions
   const [screenDimention, updateScreenDimentions] = useState({
     width: 0,
@@ -45,25 +49,77 @@ const Game = ()=>{
     };
   }, []);
 
+  useEffect(() => {
+    const handleKeydown = (e) => {
+      e.preventDefault();
+      const { key, keyCode } = e;
+      console.log(key, keyCode);
+      switch (keyCode) {
+        case 37:
+          // left
+          updateModelState('turnLeft')
+          break;
+        case 38:
+          // up
+          updateModelState('walking');
+          updateModelPos((prePos)=>{return [0,prePos[1],prePos[2] + 0.5]});
+          break
+        case 39:
+          // right
+          updateModelState('turnRight');
+          break;
+        case 40:
+          // down
+          updateModelState('backward');
+          updateModelPos((prePos)=>{return [0,prePos[1],prePos[2] - 0.5]})
+          break;
+        default:
+          updateModelState('idle')
+          break;
+      }
+    };
+    window.addEventListener('keydown', handleKeydown);
 
-    return(
-        <Canvas
-        shadows
-        style={{
-          width: "100%",
-          height: "90vh",
-          background: 'black',
-        }}
-      >
-        <PerspectiveCamera makeDefault position={[-5, 6, 50]} fov={55}/>
-        <OrbitControls />
-        <ambientLight intensity={switchState ? 0.2 : 1} />
-       
-        {/* walls and ground*/}
-        <Suspense fallback={null}>
-            <Running position={[0, -15, 5]} />
-         </Suspense>
-      </Canvas>
-    )
+    return () => {
+      window.removeEventListener('keydown', handleKeydown);
+    };
+  }, []);
+
+
+  return (
+    <Canvas
+      shadows
+      style={{
+        width: "100%",
+        height: "90vh",
+        background: 'black',
+      }}
+    >
+      <PerspectiveCamera makeDefault position={[0, 70, -40]} fov={70} />
+      <OrbitControls />
+      <ambientLight intensity={1} />
+
+      {/* walls and ground*/}
+        {(() => {
+          switch (modelState) {
+            case 'walking':
+              return <Running position={modelPos} />;
+            case 'backward':
+              return <Running position={modelPos} />;
+            case 'running':
+              return <Running position={modelPos} />;
+            case 'turningRight':
+              return <Running position={modelPos} />;
+            case 'runningLeft':
+              return <Running position={modelPos}/>;
+            case 'idle':
+              return <Model position={modelPos} />;
+            default:
+              return <Model position={modelPos} />;
+          }
+        })()}
+        <Walls/>
+    </Canvas>
+  )
 };
 export default Game;
