1ebf0b76a064065b3ccd7085fe92dd0a92ca4fef
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-09-12 07:11:22 +0000

    fully functional walking

--- src/components/content/game/model/Model.jsx
+++ src/components/content/game/model/Model.jsx
@@ -5,10 +5,9 @@ import { SkeletonUtils } from 'three-stdlib';
 
 export const Model = (props) => {
   const modelRef = useRef();
-  const [modelState, updateModelState] = useState('idle');
   const [modelPos, updateModelPos] = useState([0, -15, 0]);
   const [modelRotation, updateModelRotation] = useState([Math.PI / 2, 0, 0]);
-  const modelDirectionPercentageRef = useRef({ x: 0, y: 1 });
+  const modelDeg = useRef({ x: 0, y: 1 });
   const [modelSpeed, updateModelSpeed] = useState(.6);
   const { scene, animations } = useGLTF('/models/model-walking.glb')
   const clone = useMemo(() => SkeletonUtils.clone(scene), [scene])
@@ -25,24 +24,20 @@ export const Model = (props) => {
       switch (keyCode) {
         case 37:
           // left
-          updateModelState('turnLeft');
           updateModelRotation((preDeg) => { return [preDeg[0], preDeg[1], preDeg[2] - rotationSpeed] });
           actions['Armature.001|mixamo.com|Layer0.001'].play();
           break;
         case 39:
           // right
-          updateModelState('turnLeft');
           updateModelRotation((preDeg) => { return [preDeg[0], preDeg[1], preDeg[2] + rotationSpeed] });
           actions['Armature.001|mixamo.com|Layer0.001'].play();
           break;
         case 38:
           // up
-          console.log('direction', modelDirectionPercentageRef.current);
-          updateModelSpeed((preSpeed) => { return preSpeed < maxSpeed ? preSpeed + 0.01 : preSpeed })
-          // updateModelState('walking');
-          updateModelPos((prePos) => { return [prePos[0] + (modelSpeed * (modelDirectionPercentageRef.current.x / 100)), prePos[1], prePos[2] + (modelSpeed * (modelDirectionPercentageRef.current.y) / 100)] });
-
-
+          updateModelSpeed((preSpeed) => { return preSpeed < maxSpeed ? preSpeed + 0.01 : preSpeed });
+          updateModelPos((prePos) => {
+            return [prePos[0] + modelSpeed * modelDeg.current.x, prePos[1], prePos[2] + modelSpeed * modelDeg.current.y]
+          });
           actions['Armature.001|mixamo.com|Layer0.001'].play();
 
           // start walking animation
@@ -50,7 +45,6 @@ export const Model = (props) => {
           break;
         case 40:
           // down
-          updateModelState('running');
           updateModelPos((prePos) => { return [prePos[0], prePos[1], prePos[2] - 0.5] });
           actions['Armature.002|mixamo.com|Layer0'].play();
           break;
@@ -78,52 +72,15 @@ export const Model = (props) => {
 
   // Character rotation
   useEffect(() => {
-
-    const charDeg = (modelRotation[2] * (180 / Math.PI).toFixed(0)) % 360;
-
-    const convertToPercent = (x) => {
-      const result = ((charDeg % 90) * 100) / 90;
-      if (charDeg > 0) {
-        if (charDeg <= 90) {
-          return { x: -result, y: 100 - result }
-        }
-        else if (charDeg > 90 && charDeg <= 180) {
-          return { x: -(100 - result), y: -result  }
-        }
-        else if (charDeg > 180 && charDeg <= 270) {
-          return { x: result, y: -(100 - result) }
-        }
-        else if (charDeg > 270 && charDeg <= 359) {
-          return { x: 100 - result, y:result  }
-        }
-      } else {
-        const charDegAbs = Math.abs(charDeg);
-
-        if (charDegAbs <= 90) {
-          console.log('91');
-          return { x: - result, y: 100 + result }
-        }
-        else if (charDegAbs > 90 && charDegAbs <= 180) {
-          console.log('94');
-          return { x: -result , y: -(100 + result) }
-        }
-        else if (charDegAbs > 180 && charDegAbs <= 270) {
-          console.log('93');
-          return { x:result, y:  - (100 + result) }
-        }
-        else if (charDegAbs > 270 && charDegAbs <= 359) {
-          console.log('92');
-          console.log(result);
-          
-          return { x: result, y: 100+result}
-        }
+    const deg = modelRotation[2];
+      if(deg < 90){
+        modelDeg.current = { x: -Math.sin(deg), y: Math.cos(deg) };
       }
-    }
-
-
-    // checks the rotation side right or left...
-
-    modelDirectionPercentageRef.current = convertToPercent(charDeg);
+      else if(deg >= 90 && deg < 180){
+        console.log({ x: -Math.sin(deg), y: -Math.cos(deg)});
+        modelDeg.current = { x: -Math.sin(deg), y: -Math.cos(deg) };
+      }
+    
 
   }, [modelRotation]);
 
