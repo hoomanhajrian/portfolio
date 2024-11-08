b84629436c2d1bbfbc0b45721e14726cd1591627
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-11-08 21:34:42 +0000

    update app loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { createRoot } from "react-dom/client";
-import { Suspense } from 'react';
-import AppLoading from './components/loading/AppLoading';
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
@@ -9,6 +7,6 @@ const container = document.getElementById("app");
 const root = createRoot(container);
 
 root.render(
-    <Suspense fallback={<AppLoading />}><App /></Suspense>);
+    <App />);
 
 reportWebVitals();
