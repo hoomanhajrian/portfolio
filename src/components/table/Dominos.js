5836963ba4753e000fe327ebbc85f1e0184dac96
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-11-20 04:20:01 +0000

    dominos works individually

--- src/components/table/Dominos.js
+++ src/components/table/Dominos.js
@@ -1,33 +1,64 @@
-import { Box } from "@react-three/drei";
+import { Box, Sphere } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
 import { useRef, useState } from "react";
 
-const dominos = [{ position: { x: -12, y: 20 } },{ position: { x: -10, y: 20 } }, { position: { x: -8, y: 20 } }, { position: { x: -6, y: 20 } }, { position: { x: -4, y: 20 } }, { position: { x: -2, y: 20 } }];
+const dominos = [
+    { id: 0, position: { x: -8, y: 20 } },
+    { id: 1, position: { x: -6, y: 20 } },
+    { id: 2, position: { x: -4, y: 20 } },
+    { id: 3, position: { x: -2, y: 20 } },
+    { id: 4, position: { x: 0, y: 20 } },
+    { id: 5, position: { x: 2, y: 20 } },
+    { id: 6, position: { x: 4, y: 20 } },
+    { id: 7, position: { x: 6, y: 20 } },
+    { id: 8, position: { x: 8, y: 20 } },
+    { id: 9, position: { x: 10, y: 20 } }
+];
 
 export const Dominos = () => {
-    const boxRef = useRef();
-    const [hovered,hover] = useState();
+    const [hovered, hover] = useState(null);
+    const boxRef = useRef({});
 
-    const jumpRight = () => {
-        boxRef.current.applyImpulse({ x: -2, y: 0, z: 0 });
+    const jumpRight = (id) => {
+        const rigidBody = boxRef.current[id];
+        if (rigidBody) {
+            rigidBody.applyImpulse({ x: -2, y: 0, z: 0 }, true); // Apply impulse
+        }
     };
 
     return (
         <>
-            {dominos.map((item, index) => {
+            {dominos.map((item) => {
                 return (
-                    <RigidBody friction={1} position={[item.position.x, -6, item.position.y]} ref={boxRef} key={index} colliders={'cuboid'}>
-                        <Box onPointerEnter={()=>{hover(true)}} onPointerLeave={()=>{hover(false)}} onClick={jumpRight} castShadow receiveShadow args={[.5, 3, 1]}>
-                            <meshStandardMaterial color={hovered?"lightgreen":'darkGreen'} />
+                    <RigidBody
+                        key={item.id}
+                        friction={.3}
+                        position={[item.position.x, -6, item.position.y]}
+                        ref={(el) => (boxRef.current[item.id] = el)} // Store refs
+                        colliders={"cuboid"}>
+                        <Box
+                            onPointerEnter={() => hover(item.id)} // Track hover
+                            onPointerLeave={() => hover(null)}
+                            onClick={() => jumpRight(item.id)} // Pass function reference
+                            castShadow
+                            receiveShadow
+                            args={[0.5, 3, 1]}>
+                            <meshStandardMaterial color={hovered ? "lightgreen" : 'darkGreen'} />
                         </Box>
                     </RigidBody>)
             })}
-
-            {/* <RigidBody colliders={'cuboid'} type='dynamic'>
-                <Box position={[-9, 0, 15]} castShadow receiveShadow args={[1, 3, 1]}>
-                    <meshStandardMaterial color={'darkGreen'} />
-                </Box>
-            </RigidBody> */}
+            <RigidBody
+                position={[-12, -6, 20]}
+                colliders={'ball'}
+                type="dynamic"
+                >
+                <Sphere
+                    castShadow
+                    receiveShadow
+                    args={[1.3, 360, 360]}>
+                    <meshStandardMaterial color={'darkgreen'} />
+                </Sphere>
+            </RigidBody>
         </>
     )
 };
